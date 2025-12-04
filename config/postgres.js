2412d2a492c6a056cbacb1478b4e4586d6c82d4f
sbahri7 <satyambahri12345@gmail.com>
2025-12-04 14:28:07 -0500

    Fix pg config for Vercel deployment

--- config/postgres.js
+++ config/postgres.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require("sequelize");
- const pg = require("pg"); 
+const pg = require("pg");   // ensure this line exists
+
 const sequelize = new Sequelize(process.env.PG_URI, {
   dialect: "postgres",
+  dialectModule: pg,        // ensure this line exists
   dialectOptions: {
     ssl: {
       require: true,
